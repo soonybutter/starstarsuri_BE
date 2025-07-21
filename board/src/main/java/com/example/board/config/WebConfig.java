225b386076d892cac54545f4641451d81b2d2f78
Yang Dayeon <ydy229@naver.com>
2025-07-21 17:43:22 +0900

    [BE] FrontEndController 추가, WebConfig.java 경로 수정

--- board/src/main/java/com/example/board/config/WebConfig.java
+++ board/src/main/java/com/example/board/config/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**") // 모든 경로 
+        registry.addMapping("/api/**") // 모든 경로 
                 .allowedOrigins("http://localhost:3000", "http://20.41.123.6") // 프론트 주소
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
